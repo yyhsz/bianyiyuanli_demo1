456171e960044b873d096451774e2fa4dd42ed41
余银海 <591985864@qq.com>
2021-02-13 10:58:25 +0800

    first commit

---
+++ lexer.js
@@ -0,0 +1,11 @@
+// const 
+export const tokenzier = (code) => {
+
+  let tokens = []
+  //定义
+  const Let = 'let'
+  const Id = /^[a-zA-Z_]([a-zA-Z_]|[0-9])*$/
+  for (let i = 0; i < code.length; i++) {
+    
+  }
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+const simple1 = `
+  let a = 1 
+  let b = a + 1
+`
